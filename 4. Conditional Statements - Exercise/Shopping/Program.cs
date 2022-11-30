77cd024568a089559d7c5d3bace36178f7e8f411
Ivan Ivanov <ivan78608@gmail.com>
2022-11-30 03:10:34 +0200

    Solved Exercise - 7. Shopping

--- 4. Conditional Statements - Exercise/Shopping/Program.cs
+++ 4. Conditional Statements - Exercise/Shopping/Program.cs
@@ -6,9 +6,38 @@ namespace Shopping
     {
         static void Main(string[] args)
         {
-            // Video Lecture is on 1:27:28 from 2:42:00
-            // 7. Shopping
+            double peturBudget = double.Parse(Console.ReadLine());
+            int gpuAmount = int.Parse(Console.ReadLine());
+            int cpuAmount = int.Parse(Console.ReadLine());
+            int ramAmount = int.Parse(Console.ReadLine());
 
+            double gpuPrice = gpuAmount * 250;
+            double cpuPrice = (gpuPrice * cpuAmount) * 0.35;
+            double ramPrice = (gpuPrice * ramAmount) * 0.10;
+
+            double totalPrice = gpuPrice + cpuPrice + ramPrice;
+
+            //double discountedTotalPrice = 0.0;
+            if (gpuAmount > cpuAmount)
+            {
+                //discountedTotalPrice = totalPrice - (totalPrice * 0.15);
+                totalPrice -= totalPrice * 0.15;
+            }
+
+            double remainingBudget = peturBudget - totalPrice;
+            double neededBudget = totalPrice - peturBudget;
+
+            if (totalPrice <= peturBudget)
+            {
+                Console.WriteLine($"You have {remainingBudget:F2} leva left!");
+            }
+            else if (totalPrice > peturBudget)
+            {
+                Console.WriteLine($"Not enough money! You need {neededBudget:F2} leva more!");
+            }
+            
+
+           
         }
     }
 }
