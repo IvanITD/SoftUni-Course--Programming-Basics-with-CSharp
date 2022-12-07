4bfda4df5bf7e800f6a38943b38c1ee26701c05b
Ivan Ivanov <ivan78608@gmail.com>
2022-12-07 11:15:34 +0200

    Exercise Solved Day of Week - Conditional Statements Advanced

--- 5. Conditional Statements Advance - Lab/DayOfWeek/Program.cs
+++ 5. Conditional Statements Advance - Lab/DayOfWeek/Program.cs
@@ -6,7 +6,40 @@ namespace DayOfWeek
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int dayOfWeek = int.Parse(Console.ReadLine());
+
+            if (dayOfWeek == 1)
+            {
+                Console.WriteLine("Monday");
+            }
+            else if (dayOfWeek == 2)
+            {
+                Console.WriteLine("Tuesday");
+            }
+            else if (dayOfWeek == 3)
+            {
+                Console.WriteLine("Wednesday");
+            }
+            else if (dayOfWeek == 4)
+            {
+                Console.WriteLine("Thursday");
+            }
+            else if (dayOfWeek == 5)
+            {
+                Console.WriteLine("Friday");
+            }
+            else if (dayOfWeek == 6)
+            {
+                Console.WriteLine("Saturday");
+            }
+            else if (dayOfWeek == 7)
+            {
+                Console.WriteLine("Sunday");
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
         }
     }
 }
