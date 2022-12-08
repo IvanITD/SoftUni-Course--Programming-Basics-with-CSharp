27c83e04644ef6f9a26fd12f27d8a61739ad705e
Ivan Ivanov <ivan78608@gmail.com>
2022-12-08 02:25:07 +0200

    Exercise Solved Cinema Ticket

---
+++ 5. Conditional Statements Advance - Lab/CinemaTicket/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CinemaTicket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string weekDay = Console.ReadLine();
+
+            if (weekDay == "Monday")
+            {
+                Console.WriteLine("12");
+            }
+            else if (weekDay == "Tuesday")
+            {
+                Console.WriteLine("12");
+            }
+            else if (weekDay == "Wednesday")
+            {
+                Console.WriteLine("14");
+            }
+            else if (weekDay == "Thursday")
+            {
+                Console.WriteLine("14");
+            }
+            else if (weekDay == "Friday")
+            {
+                Console.WriteLine("12");
+            }
+            else if (weekDay == "Saturday")
+            {
+                Console.WriteLine("16");
+            }
+            else if (weekDay == "Sunday")
+            {
+                Console.WriteLine("16");
+            }
+        }
+    }
+}
