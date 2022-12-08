74dbbf646d410fb5c75e5957781ef5529e9c5fff
Ivan Ivanov <ivan78608@gmail.com>
2022-12-08 02:11:08 +0200

    Last written Solution

--- 5. Conditional Statements Advance - Lab/WorkingHours/Program.cs
+++ 5. Conditional Statements Advance - Lab/WorkingHours/Program.cs
@@ -2,20 +2,88 @@
 
 namespace WorkingHours
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            int hour = int.Parse(Console.ReadLine());
+            int dayHour = int.Parse(Console.ReadLine());
             string weekDay = Console.ReadLine();
 
-            if ((hour >= 10 && hour <= 18) && weekDay != "Sunday")
+            switch (weekDay)
             {
-                Console.WriteLine("open");
-            }
-            else
-            {
-                Console.WriteLine("closed");
+                case "Monday":
+                    if (dayHour >= 10 && dayHour <= 18)
+                    {
+                        Console.WriteLine("open");
+                    }
+                    else
+                    {
+                        Console.WriteLine("closed");
+                    }
+                    break;
+
+                case "Tuesday":
+                    if (dayHour >= 10 && dayHour <= 18)
+                    {
+                        Console.WriteLine("open");
+                    }
+                    else
+                    {
+                        Console.WriteLine("closed");
+                    }
+                    break;
+
+                case "Wednesday":
+                    if (dayHour >= 10 && dayHour <= 18)
+                    {
+                        Console.WriteLine("open");
+                    }
+                    else
+                    {
+                        Console.WriteLine("closed");
+                    }
+                    break;
+
+                case "Thursday":
+                    if (dayHour >= 10 && dayHour <= 18)
+                    {
+                        Console.WriteLine("open");
+                    }
+                    else
+                    {
+                        Console.WriteLine("closed");
+                    }
+                    break;
+
+                case "Friday":
+                    if (dayHour >= 10 && dayHour <= 18)
+                    {
+                        Console.WriteLine("open");
+                    }
+                    else
+                    {
+                        Console.WriteLine("closed");
+                    }
+                    break;
+
+                case "Saturday":
+                    if (dayHour >= 10 && dayHour <= 18)
+                    {
+                        Console.WriteLine("open");
+                    }
+                    else
+                    {
+                        Console.WriteLine("closed");
+                    }
+                    break;
+
+                case "Sunday":
+                    if (dayHour >= 10 && dayHour <= 18)
+                    {
+                        Console.WriteLine("closed");
+                    }
+                    break;
+                    
             }
         }
     }
