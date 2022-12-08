01f7acfe45f0660cf20cdff2b36de52b775c4750
Ivan Ivanov <ivan78608@gmail.com>
2022-12-08 22:46:18 +0200

    Exercise Solved with if construction Fruit Shop (If Construction)

--- 5. Conditional Statements Advance - Lab/Invalid Number-Bool/Program.cs
+++ 5. Conditional Statements Advance - Lab/Invalid Number-Bool/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
-namespace Invalid_Number_Bool
+namespace InvalidNumber_Bool_
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            
+            int num = int.Parse(Console.ReadLine());
+
+            bool isValid = (num >= 100 && num <= 200) || num == 0;
+
+            if (!isValid)
+            {
+                Console.WriteLine("invalid");
+            }
         }
     }
 }
