66ff50eee0a7f0f243c01ac6db5b429c91026471
Ivan Ivanov <ivan78608@gmail.com>
2022-12-19 09:06:43 +0200

    changed name

---
+++ 6. ConditionalStatementsAdvanced-Exercise/SummerOu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SummerOu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
