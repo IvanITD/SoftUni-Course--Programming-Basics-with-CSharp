55c768c68d16fea5d0fafe5573068d17719ed0d2
Ivan Ivanov <ivan78608@gmail.com>
2022-12-26 01:56:56 +0200

    Not Yet Solved 46/100 Still yet to be solved

---
+++ 6. ConditionalStatementsAdvanced-Exercise/FishingBoat/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace FishingBoat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int groupBudget = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            int fishersAmount = int.Parse(Console.ReadLine());
+
+            double rentPrice = 0.00;
+
+            if (season == "Spring")
+            {
+                rentPrice = 3000;
+                if (fishersAmount <= 6)
+                {
+                    rentPrice -= rentPrice * 0.10;
+                    if (fishersAmount % 2 == 0)
+                    {
+                        rentPrice -= rentPrice * 0.5;
+                    }
+                }
+                else if (fishersAmount >= 7 && fishersAmount <= 11)
+                {
+                    rentPrice -= rentPrice * 0.15;
+                    if (fishersAmount % 2 == 0)
+                    {
+                        rentPrice -= rentPrice * 0.5;
+                    }
+                }
+                else if (fishersAmount > 12)
+                {
+                    rentPrice -= rentPrice * 0.25;
+                    if (fishersAmount % 2 == 0)
+                    {
+                        rentPrice -= rentPrice * 0.5;
+                    }
+                }
+                
+            }
+            else if (season == "Summer")
+            {
+                rentPrice = 4200;
+                if (fishersAmount <= 6)
+                {
+                    rentPrice -= rentPrice * 0.10;
+                    if (fishersAmount % 2 == 0)
+                    {
+                        rentPrice -= rentPrice * 0.5;
+                    }
+                }
+                else if (fishersAmount >= 7 && fishersAmount <= 11)
+                {
+                    rentPrice -= rentPrice * 0.15;
+                    if (fishersAmount % 2 == 0)
+                    {
+                        rentPrice -= rentPrice * 0.5;
+                    }
+                }
+                else if (fishersAmount > 12)
+                {
+                    rentPrice -= rentPrice * 0.25;
+                    if (fishersAmount % 2 == 0)
+                    {
+                        rentPrice -= rentPrice * 0.5;
+                    }
+                }
+               
+            }
+            else if (season == "Autumn")
+            {
+                rentPrice = 4200;
+                if (fishersAmount <= 6)
+                {
+                    rentPrice -= rentPrice * 0.10;
+                }
+                else if (fishersAmount >= 7 && fishersAmount <= 11)
+                {
+                    rentPrice -= rentPrice * 0.15;
+                }
+                else if (fishersAmount > 12)
+                {
+                    rentPrice -= rentPrice * 0.25;
+                }
+            }
+            else if (season == "Winter")
+            {
+                rentPrice = 2600;
+                if (fishersAmount <= 6)
+                {
+                    rentPrice -= rentPrice * 0.10;
+                    if (fishersAmount % 2 == 0)
+                    {
+                        rentPrice -= rentPrice * 0.5;
+                    }
+                }
+                else if (fishersAmount >= 7 && fishersAmount <= 11)
+                {
+                    rentPrice -= rentPrice * 0.15;
+                    if (fishersAmount % 2 == 0)
+                    {
+                        rentPrice -= rentPrice * 0.5;
+                    }
+                }
+                else if (fishersAmount > 12)
+                {
+                    rentPrice -= rentPrice * 0.25;
+                    if (fishersAmount % 2 == 0)
+                    {
+                        rentPrice -= rentPrice * 0.5;
+                    }
+                }
+            }
+
+            double remainingMoney = groupBudget - rentPrice;
+            double neededMoney = rentPrice - groupBudget;
+
+            if (rentPrice >= groupBudget)
+            {
+                Console.WriteLine($"Not enough money! You need {Math.Abs(neededMoney):F2} leva.");
+            }
+            else
+            {
+                Console.WriteLine($"Yes! You have {remainingMoney:F2} leva left.");
+            }
+        }
+    }
+}
