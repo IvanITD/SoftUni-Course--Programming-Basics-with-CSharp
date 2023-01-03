80ae00ae3dc1711b310c3085f08d55db514aadbd
Ivan Ivanov <ivan78608@gmail.com>
2023-01-03 23:22:18 +0200

    Exercise Solved Operations Between Numbers

--- 6. ConditionalStatementsAdvanced-Exercise/06/Program.cs
+++ 6. ConditionalStatementsAdvanced-Exercise/06/Program.cs
@@ -6,7 +6,73 @@ namespace OperationsBetweenNumbers
     {
         static void Main(string[] args)
         {
-            
+            double N1 = double.Parse(Console.ReadLine());
+            double N2 = double.Parse(Console.ReadLine());
+            char simbol = char.Parse(Console.ReadLine());
+
+            double result = 0.00;
+
+            // The type of operator depends by the User's given simbol.
+            if (simbol == '+')
+            {
+                result = N1 + N2;
+                if (result % 2 == 0)
+                {
+                    Console.WriteLine($"{N1} + {N2} = {result} - even");
+                }
+                else
+                {
+                    Console.WriteLine($"{N1} + {N2} = {result} - odd");
+                }
+            }
+            else if (simbol == '-')
+            {
+                result = N1 - N2;
+                if (result % 2 == 0)
+                {
+                    Console.WriteLine($"{N1} - {N2} = {result} - even");
+                }
+                else
+                {
+                    Console.WriteLine($"{N1} - {N2} = {result} - odd");
+                }
+            }
+            else if (simbol == '*')
+            {
+                result = N1 * N2;
+                if (result % 2 == 0)
+                {
+                    Console.WriteLine($"{N1} * {N2} = {result} - even");
+                }
+                else
+                {
+                    Console.WriteLine($"{N1} * {N2} = {result} - odd");
+                }
+            }
+            else if (simbol == '/')
+            {
+                result = N1 / N2;
+                if (N2 == 0)
+                {
+                    Console.WriteLine($"Cannot divide {N1} by zero");
+                }
+                else
+                {
+                    Console.WriteLine($"{N1} / {N2} = {result:F2}");
+                }
+            }
+            else if (simbol == '%')
+            {
+                result = N1 % N2;
+                if (N2 == 0)
+                {
+                    Console.WriteLine($"Cannot divide {N1} by zero");
+                }
+                else
+                {
+                    Console.WriteLine($"{N1} % {N2} = {result}");
+                }
+            }
         }
     }
 }
